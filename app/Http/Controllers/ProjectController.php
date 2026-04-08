752f0a21700b24bdfdd9d1b967fa06d3b7a85f1d
Egor <egorker85@gmail.com>
2026-04-08 18:26:38 +0300

    feat: install laravel breeze with vue and inertia

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,11 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\Project;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProjectController extends Controller
 {
-    public function index() {
-        $project = Project::all();
-        dd($project);
+
+    public function store(Request $request){
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        Project::create([
+            'user_id' => 1,
+            'title' => $validated['title'],
+            'description' => $validated['description'] ?? null,
+        ]);
+
+        return 'project created';
     }
+
 }
