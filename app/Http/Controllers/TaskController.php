7b8bf3a7c074b4eea8cc9728a5c53dcfebb84143
Egor <egorker85@gmail.com>
2026-04-13 19:41:11 +0300

    updete reorder function

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\Task;
 use App\Models\Project;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TaskController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
     }
 
@@ -39,7 +41,7 @@ class TaskController extends Controller
     public function update(Request $request, $id)
     {
         $task = Task::where('id', $id)
-            ->whereHas('project', function($query) {
+            ->whereHas('project', function ($query) {
                 $query->where('user_id', auth()->id());
             })->firstOrFail();
 
@@ -71,24 +73,17 @@ class TaskController extends Controller
 
     public function reorder(Request $request)
     {
-        // Это остановит выполнение и выведет данные в окно браузера/консоль
-        // Если при перетаскивании ты увидишь окно с данными — значит фронт работает.
-        //dd($request->all());
+        $tasks = $request->input('tasks');
 
-        $data = $request->validate([
-            'tasks' => 'required|array',
-            'tasks.*' => 'numeric', // упростим пока проверку
-        ]);
-
-        foreach ($data['tasks'] as $index => $taskId) {
-            // Используем find, чтобы точно убедиться в обновлении
-            $task = Task::find($taskId);
-            if ($task) {
-                $task->order = $index;
-                $task->save();
+        DB::transaction(function () use ($tasks) {
+            foreach ($tasks as $task) {
+                Task::where('id', $task['id'])->update([
+                    'order' => $task['order'],
+                    'status' => $task['status'],
+                ]);
             }
-        }
+        });
 
-        return redirect()->back();
+        return back();
     }
 }
